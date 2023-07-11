ce377cca4ad342919095a4500c0a12ad711d97a9
ali3030 <ali3030@users.noreply.github.com>
2023-07-11 21:59:13 +0300

    fixing add quantity

--- app/src/main/java/com/example/dshop/screen/detailsScreen/detailsScreen.kt
+++ app/src/main/java/com/example/dshop/screen/detailsScreen/detailsScreen.kt
@@ -1,7 +1,15 @@
 package com.example.dshop.screen.detailsScreen
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -28,6 +36,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Black
@@ -69,6 +79,15 @@ fun DetailsContent(state: DetailsUIState, viewModel: DetailsViewModel) {
     ) {
 
         val (bottomSheet, mainImage, iconFav) = createRefs()
+        val infiniteTransition = rememberInfiniteTransition()
+        val angle by infiniteTransition.animateFloat(
+            initialValue = 0F,
+            targetValue = 360F,
+            animationSpec = infiniteRepeatable(
+                animation = tween(2000, easing = LinearEasing),
+                repeatMode = RepeatMode.Restart
+            )
+        )
 
         BackButton()
 
@@ -77,6 +96,7 @@ fun DetailsContent(state: DetailsUIState, viewModel: DetailsViewModel) {
             contentDescription = "donut",
             contentScale = ContentScale.FillWidth,
             modifier = Modifier.fillMaxWidth()
+                .rotate(angle)
                 .constrainAs(mainImage){
                     top.linkTo(parent.top,28.dp)
                 }
@@ -180,14 +200,14 @@ fun DetailsContent(state: DetailsUIState, viewModel: DetailsViewModel) {
 
 
             Row(
-                modifier = Modifier.padding(horizontal = 40.dp, vertical = 25.dp),
+                modifier = Modifier.padding(horizontal = 40.dp, vertical = 10.dp),
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Box(
                     modifier = Modifier
                         .width(100.dp)
-                        .padding(end = 26.dp)
+                        .padding(end = 10.dp)
                 ) {
                     Text(
                         text = "£${state.price}",
